fa6b93e3daef8932702ea15b4fd796841a79ca34
Vinicius <viniciusrvk@gmail.com>
2017-11-25 16:32:29 -0300

    filme.h e filme.cpp

--- headers/filme.h
+++ headers/filme.h
@@ -5,17 +5,33 @@
 #ifndef FILME_H
 #define FILME_H
 
-#include "video.h"
-#include "categoria.h"
+#include "../headers/video.h"
+#include <iostream>
+
 
 class Filme : Video
 {
   public:
-    Filme();
+    Filme(int new_id,
+      string new_nome,
+      string new_descricao,
+      string new_duracao,
+      Date new_data_lancamento,
+      vector<string> new_categoria, 
+      string diretor,
+      vector<string> elenco);
 
+    /*
+    *@brief Função sobrecarregada para mostrar este objeto
+    */
     void printVideo();
     void setVideo();
     Video getVideo();
+    
+    /*
+    *@brief função para mostrar o elenco
+    */
+    void showElenco(); 
 
   private:
     string diretor;
--- src/filme.cpp
+++ src/filme.cpp
@@ -7,22 +7,40 @@
 /*
  *@brief Construtor de objeto Filme
  */
-Filme::Filme(){
-	// fazer
-}
+Filme::Filme(int new_id,
+		      string new_nome,
+		      string new_descricao,
+		      string new_duracao,
+		      Date new_data_lancamento,
+		      vector<string> new_categoria, 
+		      string diretor,
+		      vector<string> elenco):
+		Video{new_id, 
+      		new_nome, 
+      		new_descricao, 
+      		new_duracao, 
+      		new_data_lancamento,
+      		new_categoria},
+      	elenco(elenco),
+      	diretor(diretor)
+	{}
 
 void Filme::printVideo(){
 	std::cout << "Título: " << getNome() << endl;
 	std::cout << "Duração: " << getDuracao() << endl;
 	std::cout << "Descrição: \n" << getDescricao() << endl;
-	std::cout << "Data: " << getData() << endl;
 	std::cout << "Diretor: " << diretor << std::endl;
-	std::cout << "Elenco: " << std::endl;
-	for(int i = 0; i < elenco.size(); i++){
-		std::cout << elenco[i] << std::endl; // pode mudar pra for each
+	showElenco();
+}
+void Filme::setVideo(){
+	
+}
+void Filme::showElenco(){
+	std::cout << "Elenco:" << std::endl;
+	for (int i = 0; i < elenco.size(); i++){
+		std::cout << elenco[i] << std::endl;
 	}
 }
-void Filme::setVideo();
 Video Filme::getVideo(){
 	return this->getVideo();
 }
